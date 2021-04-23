6b3e9e2165fae50328231e3b231c7315ea7da954
ramonis567 <ramonmatosferreira@gmail.com>
2021-04-23 09:54:59 -0300

    nunjucks

--- src/server.js
+++ src/server.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3030;
+
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/pages", {
+  express: app,
+  noCache: true,
+});
+
+//Configuração de pasta pública
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  return res.render("index.html");
+});
+
+app.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
+});
+
+app.get("/search-results", (req, res) => {
+  return res.render("search-results.html");
+});
 
 app.listen(port);
